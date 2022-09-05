56830cd0125dd645564105bbcf48b0cd0d564565
Jordi-Cordoba <jordimanuelcordoba@gmail.com>
2022-09-05 12:47:01 +0200

    finalizado

---
+++ Ejercicio9.java
@@ -0,0 +1,17 @@
+
+public class Ejercicio9 {
+
+	public static void main(String[] args) {
+
+		String nombre = "Jordix";
+		String apellido = "Cordobax";
+		String calle = "Sants";
+		String municipio = "Barcelona";
+		String correo = "jorsdfdsf@gmail.com";
+		int codigoPostal = 8014;
+		String sexo = "h";
+		System.out.println(nombre  +"\n"+ apellido +"\n"+ calle +"\n"+ municipio +"\n"+ codigoPostal +"\n"+ sexo +"\n"+ correo);
+        
+	}
+
+}
