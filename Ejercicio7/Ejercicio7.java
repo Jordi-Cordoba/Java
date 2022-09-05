faff0492bc2029f3bec91d74f27d033dda06ea59
Jordi-Cordoba <jordimanuelcordoba@gmail.com>
2022-09-05 13:22:00 +0200

    finalizado

---
+++ Ejercicio7/Ejercicio7.java
@@ -0,0 +1,19 @@
+
+public class Ejercicio7 {
+	public static void main(String[] args) {
+
+		final byte NUMERO = (byte) 133;
+		final short $NUMERO = (short) 32793;
+		final int _NUMERO = (int) 2147493648l;
+		final long NUMERO1 = (long) 9223372036854795808f;
+
+		System.out.println(NUMERO);
+		System.out.println($NUMERO);
+		System.out.println(_NUMERO);
+		System.out.println(NUMERO1);
+	}
+
+}
+
+
+
